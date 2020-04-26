6c613a47d54127b751994844aaf92607eea376cb
ieiriyuki <ieiriyuki@sur.bbiq.jp>
2020-04-26 19:18:32 +0900

    Update access key is used

--- project5/plugins/operators/stage_redshift.py
+++ project5/plugins/operators/stage_redshift.py
@@ -1,3 +1,4 @@
+from airflow.contrib.hooks.aws_hook import AwsHook
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import BaseOperator
 from airflow.utils.decorators import apply_defaults
@@ -7,7 +8,8 @@ class StageToRedshiftOperator(BaseOperator):
     COPY_TEMPLATE = """
         copy {}
         from '{}'
-        iam_role '{}'
+        access_key_id '{}'
+        secret_access_key '{}'
         region '{}'
         json '{}'
         compupdate off
@@ -16,29 +18,32 @@ class StageToRedshiftOperator(BaseOperator):
     @apply_defaults
     def __init__(self,
                  redshift_conn_id='',
+                 aws_conn_id='',
                  table='',
                  data='',
-                 arn='',
                  region='',
                  json_option='',
                  *args, **kwargs):
 
         super(StageToRedshiftOperator, self).__init__(*args, **kwargs)
         self.redshift_conn_id = redshift_conn_id
+        self.aws_conn_id = aws_conn_id
         self.table = table
         self.data = data
-        self.arn = arn
         self.region = region
         self.json_option = json_option
 
     def execute(self, context):
+        aws_hook = AwsHook(self.aws_conn_id)
+        credentials = aws_hook.get_credentials()
         redshift_hook = PostgresHook(self.redshift_conn_id)
 
         self.log.info('Execute StageToRedshiftOperator')
         sql = StageToRedshiftOperator.COPY_TEMPLATE.format(
             self.table,
             self.data,
-            self.arn,
+            credentials.access_key,
+            credentials.secret_key,
             self.region,
             self.json_option
         )
